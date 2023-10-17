bd8f71e345f6acba7d244872b7026e4ff02343fa
BT_1N3_03 <example@bths.edu>
2023-10-17 10:09:40 -0400

    Commiting first 3 methods and constructor

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+    // instance variables
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public double distance() {
+        return Math.round(100 * Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))) / 100.0;
+    }
+
+    public double slope() {
+        return Math.round(100 * ((double)(y2 - y1) / (x2 - x1))) / 100.0;
+    }
+    public double yIntercept() {
+        return  Math.round(100 * (slope() * -x1 + y1)) / 100.0;
+    }
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class LinearEquationRunner {
+public class LinearEquationRunner {
+    public static void main(String[] args) {
+        LinearEquation test = new LinearEquation(-1,5,3,10);
+        System.out.println(test.slope());
+        System.out.println(test.yIntercept());
+    }
 }
