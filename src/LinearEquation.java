99ca5e30f22f0c390d65195c2e108821d5069453
BT_1N3_03 <example@bths.edu>
2023-10-17 10:52:23 -0400

    Two more methods created

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -22,4 +22,35 @@ public class LinearEquation {
     public double yIntercept() {
         return  Math.round(100 * (slope() * -x1 + y1)) / 100.0;
     }
+    public String equation() {
+        if (slope() == 0 && yIntercept() % 1 == 0) {
+            // checking for horizontal slope of 0 & whole number intercept
+            return "y = " + (int)yIntercept();
+        } else if (slope() == 0) {
+            // checking for horizontal slope of 0 & not whole number intercept
+            return "y = " + (int)yIntercept();
+        } else if (yIntercept() % 1 == 0) {
+            // checking for whole number intercept
+            return "y = " + fractionMaker((y2 - y1), (x2 - x1)) + "x + " + (int)yIntercept();
+        } else {
+            // all other cases
+            return "y = " + fractionMaker((y2 - y1), (x2 - x1)) + "x + " + yIntercept();
+        }
+
+    }
+
+    public String fractionMaker(int numerator, int denominator) {
+        // checking for horizontal slope of 0
+        if (slope() == 0) {
+            return "";
+        }
+        // fraction making
+        if (numerator % denominator == 0 && numerator / denominator != 1) {
+            return "" + numerator;
+        } else if (numerator % denominator == 0 && numerator / denominator == 1) {
+            return "";
+        } else {
+            return numerator + "/" + denominator;
+        }
+    }
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,7 +1,8 @@
 public class LinearEquationRunner {
     public static void main(String[] args) {
-        LinearEquation test = new LinearEquation(-1,5,3,10);
+        LinearEquation test = new LinearEquation(-1,5,3,5);
         System.out.println(test.slope());
         System.out.println(test.yIntercept());
+        System.out.println(test.equation());
     }
 }
